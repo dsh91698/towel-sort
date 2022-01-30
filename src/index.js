ea7d0f909cc5ee20f27f81e81b3b9183085a207e
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2022-01-30 19:37:49 +0400

    feat: towel-sort implemented

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+  var rez = [];
+  if(!matrix) {return rez}
+
+  matrix.forEach( (el, index) => {
+    if (!(index % 2)) {
+     // console.log('! case', index, el);
+      rez = rez.concat(el);
+    //  console.log(rez);
+
+    } else if (index % 2) {
+   //   console.log('case', index, el);
+      rez = rez.concat(el.reverse());
+   //   console.log(rez);
+
+    }
+
+  });
+  return rez;
+
+};
